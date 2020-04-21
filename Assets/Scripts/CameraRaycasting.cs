c40e1a23d4836d1205d9204f1a27a305ecc32c21
LouisChrr <55202347+LouisChrr@users.noreply.github.com>
2020-04-21 12:09:26 +0200

    playerMovement & interaction

---
+++ Assets/Scripts/CameraRaycasting.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycasting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float range;
+    private IInteractable currentTarget;
+    private Camera mainCamera;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastForInteractable();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTarget != null)
+            {
+                currentTarget.OnInteract();
+            }
+        }
+
+    }
+
+    private void RaycastForInteractable()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit whatIsHit;
+
+        if (Physics.Raycast(ray, out whatIsHit, range))
+        {
+            IInteractable interactable = whatIsHit.collider.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                if (whatIsHit.distance <= interactable.MaxRange)
+                {
+                    if (interactable == currentTarget)
+                    {
+                        return;
+                    }
+                    else if (currentTarget != null)
+                    {
+                        currentTarget.OnEndHover();
+                        currentTarget = interactable;
+                        currentTarget.OnStartHover();
+                        return;
+                    }
+                    else
+                    {
+                        currentTarget = interactable;
+                        currentTarget.OnStartHover();
+                        return;
+                    }
+                }
+                else
+                {
+                    if (currentTarget != null)
+                    {
+                        currentTarget.OnEndHover();
+                        currentTarget = null;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                if (currentTarget != null)
+                {
+                    currentTarget.OnEndHover();
+                    currentTarget = null;
+                    return;
+                }
+            }
+        }
+        else
+        {
+            if (currentTarget != null)
+            {
+                currentTarget.OnEndHover();
+                currentTarget = null;
+                return;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/CubeDestroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDestroy : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    public float MaxRange { get { return maxRange; } } // Range d'interact personnel à l'objet
+    public float maxRange;
+
+    public Material highlightMat, defaultMat;
+
+    // Les 3 fonctions IInteractable à implementer 
+    public void OnStartHover()
+    {
+        GetComponent<Renderer>().material = highlightMat;
+    }
+
+    public void OnInteract()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnEndHover()
+    {
+        GetComponent<Renderer>().material = defaultMat;
+    }
+
+
+
+
+}
