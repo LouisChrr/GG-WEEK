4b20785b6ed21aab7a8bd96a4b03b5089ee2885b
Leo Nimsgern <nimsgernleo@gmail.com>
2020-04-22 12:17:07 +0200

    Ajout des sripts interaction

---
+++ Assets/Scripts/TongueStroke.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueStroke : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    public float MaxRange { get { return maxRange; } } // Range d'interact personnel à l'objet
+    public float maxRange;
+    public Animation animationTongue;
+    public Animation animationInteract;
+    public Material highlightMat, defaultMat;
+
+    // Les 3 fonctions IInteractable à implementer 
+    public void OnStartHover()
+    {
+        GetComponent<Renderer>().material = highlightMat;
+    }
+
+    public void OnInteract()
+    {
+        
+    }
+
+    public void OnEndHover()
+    {
+        GetComponent<Renderer>().material = defaultMat;
+    }
+
+}
---
+++ Assets/Scripts/TongueStrokeDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueStrokeDestroy : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    public float MaxRange { get { return maxRange; } } // Range d'interact personnel à l'objet
+    public float maxRange;
+    public Animation animationTongue;
+    public Material highlightMat, defaultMat;
+
+    // Les 3 fonctions IInteractable à implementer 
+    public void OnStartHover()
+    {
+        GetComponent<Renderer>().material = highlightMat;
+    }
+
+    public void OnInteract()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnEndHover()
+    {
+        GetComponent<Renderer>().material = defaultMat;
+    }
+
+}
