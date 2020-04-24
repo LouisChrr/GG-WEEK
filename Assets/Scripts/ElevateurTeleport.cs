eb80e2146c0973b336b643c7d0eef6f52ef82eae
Leo Nimsgern <nimsgernleo@gmail.com>
2020-04-24 16:15:01 +0200

    push song

---
+++ Assets/Scripts/ElevateurTeleport.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ElevateurTeleport : MonoBehaviour, IInteractable
+{
+    public TMP_InputField codeInputField;
+
+    public float MaxRange { get { return maxRange; } } // Range d'interact personnel à l'objet
+    public float maxRange;
+    public GameObject Player;
+    public Transform Teleport;
+    public bool waitingForCode;
+
+    public GameObject CodeGO;
+    public DialogueScriptableObject textSO;
+    public Material highlightMat, defaultMat;
+
+    // Les 3 fonctions IInteractable à implementer 
+    public void OnStartHover()
+    {
+        GetComponent<Renderer>().material = highlightMat;
+    }
+
+    public void OnInteract()
+    {
+        GetComponent<AudioSource>().Play();
+        Player.transform.position = Teleport.position;
+    }
+
+    public void OnEndHover()
+    {
+        GetComponent<Renderer>().material = defaultMat;
+    }
+
+ 
+
+    public void checkCode()
+    {
+
+        string code = codeInputField.text;
+
+        if (textSO.codeApres && textSO.code == code && waitingForCode)
+        {
+            
+            codeInputField.text = "";
+            CodeGO.SetActive(false);
+            waitingForCode = false;
+        }
+
+    }
+}
