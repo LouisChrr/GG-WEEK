0393c7e8d839a35118f891216d3dc0fd2f510eca
Leo Nimsgern <nimsgernleo@gmail.com>
2020-04-24 12:16:01 +0200

    TP SCRIPT

---
+++ Assets/Scripts/CubeTeleport.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTeleport : MonoBehaviour, IInteractable
+{
+    public float MaxRange { get { return maxRange; } } // Range d'interact personnel à l'objet
+    public float maxRange;
+    public GameObject Player;
+    public Transform Teleport;
+
+    public Material highlightMat, defaultMat;
+
+    // Les 3 fonctions IInteractable à implementer 
+    public void OnStartHover()
+    {
+        GetComponent<Renderer>().material = highlightMat;
+    }
+
+    public void OnInteract()
+    {
+        
+        Player.transform.position = Teleport.position;
+        Debug.Log("Interagi");
+    }
+
+    public void OnEndHover()
+    {
+        GetComponent<Renderer>().material = defaultMat;
+    }
+
+
+}
